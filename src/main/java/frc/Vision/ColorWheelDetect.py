ab5fe83fb40908b62683fc9f39b1f727e3fa85cf
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-01-06 11:43:10 -0500

    added color wheel basic

---
+++ src/main/java/frc/Vision/ColorWheelDetect.py
@@ -0,0 +1,38 @@
+import cv2 
+import numpy as np
+import ntcore
+import Logger
+
+from networktables import NetworkTables
+
+# Inits Netowktables and connects to robo Rio server
+
+NetworkTables.initialize(server='roboRIO-5740-FRC.local')
+
+
+cap = cv2.VideoCapture()
+
+cap.open('http://roboRIO-5740-FRC.local:1181/?action=stream')
+
+centers = []
+
+
+while (1):
+
+    # Take each frame
+    cap.set(3, 12)
+    _, frame = cap.read()
+
+    # First convirts BGR 2 Gray then Converts BGR 2 HSV
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cmyk = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow("colorwheel",frame)
+
+
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+
+        break
+cap.release()
+
