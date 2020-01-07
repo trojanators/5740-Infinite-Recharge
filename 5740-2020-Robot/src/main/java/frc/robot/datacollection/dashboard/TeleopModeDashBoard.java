ea7fc47fa58e5eb37cd1e66bb5e08df6e24c412c
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-01-07 09:25:52 -0500

    Started Shuffleboard Data windows

--- 5740-2020-Robot/src/main/java/frc/robot/datacollection/dashboard/TeleopModeDashBoard.java
+++ 5740-2020-Robot/src/main/java/frc/robot/datacollection/dashboard/TeleopModeDashBoard.java
@@ -1,10 +1,16 @@
 package frc.robot.datacollection.dashboard;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 /**
  * This class is for Displaying Data To the DriverStation in TeleOpMode and
  * Shows user the specific Data from Teleop mode
  */
 
 public class TeleopModeDashBoard {
+
+    // Creates Tab for Shuffleboard called TeleOp-Tab
+    public static ShuffleboardTab TeleOpTab = Shuffleboard.getTab("TeleOp-Tab");
 
 }
--- 5740-2020-Robot/src/main/java/frc/robot/datacollection/dashboard/TestModeDashboard.java
+++ 5740-2020-Robot/src/main/java/frc/robot/datacollection/dashboard/TestModeDashboard.java
@@ -1,10 +1,16 @@
 package frc.robot.datacollection.dashboard;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 /**
  * This Dashboard is Used When Robot is in TestMode Displays every data point
  * from robot in test mode
  */
 
 public class TestModeDashboard {
+
+    // Creates Tab for Shuffleboard called Test-Tab
+    public static ShuffleboardTab TestTab = Shuffleboard.getTab("Test-Tab");
 
 }
