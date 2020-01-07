039a9f8a5742d3157aebe160a0ab5c16f3ceef99
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-01-07 08:45:06 -0500

    Added auto Dashboard Display

---
+++ 5740-2020-Robot/src/main/java/frc/robot/datacollection/AutoModeDashboard.java
@@ -0,0 +1,10 @@
+package frc.robot.datacollection;
+
+/**
+ * This class is for Displaying Data To the DriverStation in AutoMode and Shows
+ * user the specific Data from Auto mode
+ */
+
+public class AutoModeDashboard {
+
+}
