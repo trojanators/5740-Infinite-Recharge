5c1f158839b8fd3bcccf8ca26896fa85c4d39957
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-01-07 08:26:56 -0500

    added and Linked Robot Hardware def class

---
+++ 5740-2020-Robot/src/main/java/frc/robot/RobotHardware.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+/**
+ * This Class is Where you define all robot side hardware (Ex:Motor Controllers
+ * , Buttons, Pdp, Pcm, Solenoids and etc)
+ */
+public class RobotHardware {
+
+}
