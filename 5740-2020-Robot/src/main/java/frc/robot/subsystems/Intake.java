2c3d127b0ac23d9401944123ef25253fe579f888
OodleOfNoodlez <Laurene12222@gmail.com>
2020-01-07 18:31:09 -0500

    Intake Pseudo

--- 5740-2020-Robot/src/main/java/frc/robot/subsystems/Intake.java
+++ 5740-2020-Robot/src/main/java/frc/robot/subsystems/Intake.java
@@ -13,12 +13,43 @@ public class Intake extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public Intake() {
+  /*
+    Private final GroundIntake m_robotIntake
+      = new GroundIntake(motor(port));
+    Private final Storage m_storage = new Storage(motor);
 
+    Private final BallCounter m_counter = new BallCounter();
+  */
+
+  public Intake() {
+    /*
+    If (m_counter < 5){
+    m_robotIntake.set(on);
+    }
+    else{
+    m_robotIntake.set(stop);
+    }
+    */
+    //starts up intake and counts the balls via motion sensor, once over 5 it stops
   }
+
+  /*
+  public void Storage(){
+    If (m_robotIntake = stop){
+      m_storage.set(on)
+    }
+    else{
+      m_storage.set(off)
+    }
+  }
+
+  */
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    /*
+      Intake.startIntake;
+    */
   }
 }
