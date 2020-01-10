2f5966a574b52f57d9328e75ad3d9e55070bf8bd
trojanators <frc5740@gmail.com>
2020-01-09 19:42:27 -0500

    add RecordPlayer class

--- 5740-2020-Robot/src/main/java/frc/robot/subsystems/RecordPlayer.java
+++ 5740-2020-Robot/src/main/java/frc/robot/subsystems/RecordPlayer.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,11 +9,15 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ControlPanel extends SubsystemBase {
+public class RecordPlayer extends SubsystemBase {
   /**
-   * Creates a new ExampleSubsystem.
+   * Creates a new RecordPlayer.
+   * Determine starting color to count rotations
+   * Define the stopping color from game data
+   * Flip out from actuator
+   * Control the driving motor
    */
-  public ControlPanel() {
+  public RecordPlayer() {
 
   }
 
@@ -22,17 +26,3 @@ public class ControlPanel extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 }
-/* Turn on RBG sensor 
- Find out what color is at the front 
- Set motor to coast mode to make the band rotate 
- Keep sensor on to track how many rotations have passed 
- Set motor in brake mode once two rotations have passed 
- Turn off RBG sensor
-*/
-/* Turn on RBG sensor
-Set motor to coast mode
-Set motor back in brake mode when it gets to the color needed
- Turn off RBG sensor */
-
- 
-
