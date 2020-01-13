82907b3bffbb6ae313a58e561d1aeb241f1e7731
nicholas blackburn <nickblackburn02@gmail.com>
2020-01-13 14:56:50 -0500

    added error strings in code

--- 5740-2020-Robot/src/main/java/frc/robot/util/CvsLoggerStrings.java
+++ 5740-2020-Robot/src/main/java/frc/robot/util/CvsLoggerStrings.java
@@ -1,7 +1,11 @@
 package frc.robot.util;
 
 public class CvsLoggerStrings {
+
     public static String Init = "Started";
     public static String calabrating = "Calibrating";
     public static String exiting = "Exiting";
+    public static String Error = "Error in";
+    public static String Major_Error = "Major Error In";
+
 }
