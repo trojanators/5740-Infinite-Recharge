cfd0ec16796fa44f6016e281697948f52030a1ee
nicholas blackburn <nickblackburn02@gmail.com>
2020-02-21 15:36:09 -0500

    added block comments to s

--- 5740-2020-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 5740-2020-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,17 +1,14 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
+/**
+ * This Class is For Controlling our Robot Drive Train
+ * it has All our Functions Organized and used in all most all of our  commands
+ * @author Luke Crum, Nicholas Blackburn
+ */
 package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
@@ -27,24 +24,6 @@ import frc.robot.util.CvsLoggerStrings;
 import frc.robot.util.PID;
 
 public class Drivetrain extends SubsystemBase {
-
-	/**
-	 * Creates a new Drivetrain
-	 */
-
-	// private final WPI_TalonSRX tfrontRDrive = new
-	// WPI_TalonSRX(Constants.FrontRightDriveCAN);
-	// private final WPI_TalonSRX tbackRDrive = new
-	// WPI_TalonSRX(Constants.BackRightDriveCAN);
-	//private final Victor frontRDrive = new Victor(2);
-	//private final Victor backRDrive = new Victor(3);
-
-	// private final WPI_TalonSRX tfrontLDrive = new
-	// WPI_TalonSRX(Constants.FrontLeftDriveCAN);
-	// private final WPI_TalonSRX tbackLDrive = new
-	// WPI_TalonSRX(Constants.BackLeftDriveCAN);
-	//private final Victor frontLDrive = new Victor(0);
-	//private final Victor backLDrive = new Victor(1);
 
 	private WPI_TalonSRX frontLDrive = new WPI_TalonSRX(Constants.leftDriveACAN);
 	private WPI_TalonSRX backLDrive = new WPI_TalonSRX(Constants.leftDriveBCAN);
@@ -67,8 +46,18 @@ public class Drivetrain extends SubsystemBase {
 	private double gyroWorkingZero = 0;
 
 	public Drivetrain() {
-		// frontRDrive.setInverted(true);
-		// backRDrive.setInverted(true);
+
+		driveTrainConfig();
+		currentLimit();
+	}
+
+	/**
+	 * Gets creates and Configures the Drivetrain used in other subsystems
+	 * 
+	 * @author Nicholas Blackburn
+	 */
+	private void driveTrainConfig() {
+
 		turnPID.setMaxOutput(1.0);
 		drivePID.setMaxOutput(1.0);
 
@@ -85,6 +74,17 @@ public class Drivetrain extends SubsystemBase {
 		backLDrive.configOpenloopRamp(Constants.kRampRate);
 		frontRDrive.configOpenloopRamp(Constants.kRampRate);
 		backRDrive.configOpenloopRamp(Constants.kRampRate);
+
+	}
+
+	/**
+	 * Limits the current of the DriveTrain's Sim Motors Using
+	 * 
+	 * @param TalonSrx Current Limiting
+	 * 
+	 * @author Luke Crumb , Nicholas Blackburn
+	 */
+	private void currentLimit() {
 
 		frontLDrive.enableCurrentLimit(true);
 		frontLDrive.configContinuousCurrentLimit(Constants.kContinuousCurrentLimit);
@@ -105,54 +105,114 @@ public class Drivetrain extends SubsystemBase {
 		backRDrive.configContinuousCurrentLimit(Constants.kContinuousCurrentLimit);
 		backRDrive.configPeakCurrentDuration(Constants.kPeakCurrentDuration);
 		backRDrive.configPeakCurrentLimit(Constants.kPeakCurrentLimit);
-
 	}
 
-	@Override
-	public void periodic() {
-	}
-
+	/**
+	 * Zeros Encoder sensors back to 0 state
+	 * 
+	 * @param Encoders
+	 * @author Nicholas Blackburn
+	 */
 	public void zeroSensors() {
 		gyro.reset();
 		rightEncoder.reset();
 		leftEncoder.reset();
-		// Logs Reseting Encoders
-		//HelixLogger.getInstance().addStringSource("Calibrating Sensors", CvsLoggerStrings.calabrating::toString);
+
 	}
 
+	/**
+	 * Cal's Gyro to current pos of robot for MAXIMUM ACCURACY
+	 * 
+	 * @param Gyro
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void calibrateGyro() {
 		gyro.calibrate();
-		
 
 	}
 
+	/**
+	 * Sets Drivetrain Power to 3% speed
+	 * 
+	 * @param Drivetrain
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void driveForwardSlowly() {
 		drive.arcadeDrive(0.35, 0);
 	}
 
-	public void setLeftRightPower(final double left, final double right) {
+	/**
+	 * 
+	 * Sets @param Drivetrain Motor Power in a simple and Organized way
+	 * 
+	 * @author Luke Crumb
+	 */
+	public void setDrivetrainPower(final double left, final double right) {
 		frontLDrive.set(left);
 		backLDrive.set(left);
 		frontRDrive.set(right);
 		backRDrive.set(right);
 	}
 
+	/**
+	 * Distance in a Simple return type The output Should Be used in A Distace
+	 * Related Method Gets
+	 * 
+	 * @param LeftEncoder
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public double leftEncoderDistance() {
 		return frontLDrive.getSelectedSensorPosition();
 	}
 
+	/**
+	 * 
+	 * Gets Distance in a Simple return type The output Should Be used in A Distace
+	 * Related Method
+	 * 
+	 * @param RightEncoder
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public double rightEncoderDistance() {
 		return frontRDrive.getSelectedSensorPosition();
 	}
 
+	/**
+	 * Gets Rate in a Simple return type The output Should Be used in A Rate Related
+	 * Method
+	 * 
+	 * @param LeftEncoder
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public double leftEncoderRate() {
 		return leftEncoder.getRate();
 	}
 
+	/**
+	 * Gets Rate in a Simple return type The output Should Be used in A Rate Related
+	 * Method
+	 * 
+	 * @param RightEncoder
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public double rightEncoderRate() {
 		return rightEncoder.getRate();
 	}
 
+	/**
+	 * Gets Encoders Rate in a Simple return type The output Should Be used in A
+	 * Rate Related Method
+	 * 
+	 * @param Encoders
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void zeroEncoders() {
 		leftEncoder.reset();
 		rightEncoder.reset();
@@ -160,14 +220,39 @@ public class Drivetrain extends SubsystemBase {
 		frontRDrive.setSelectedSensorPosition(0);
 	}
 
+	/**
+	 * Gets Gyro Yaw in a Simple return type The output Should Be used in A Double
+	 * Related Method
+	 * 
+	 * @param Gyro
+	 * 
+	 * @author Nicholas Blackburn
+	 */
+
 	public double getGyroYaw() {
 		return gyro.getAngle() - gyroWorkingZero;
 	}
 
+	/**
+	 * Sets Gyro Yaw in a Simple Double type The output Should Be used in A Double
+	 * Related Method
+	 * 
+	 * @param Gyro
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void setGyroYaw(final double yaw) {
 		gyroWorkingZero = gyro.getAngle() - yaw;
 	}
 
+	/**
+	 * Sets Motor Power to 0
+	 * 
+	 * @param Drivetrain
+	 * @param TalonSRX
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void stop() {
 		backLDrive.set(0);
 		frontLDrive.set(0);
@@ -175,11 +260,26 @@ public class Drivetrain extends SubsystemBase {
 		frontRDrive.set(0);
 	}
 
-	// Used for test mode
+	/**
+	 * 
+	 * Sets Motor Power of Front Left motor for Drivetrain
+	 * 
+	 * 
+	 * @param Drivetrain
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void setFrontLDrive(final double power) {
 		frontLDrive.set(power);
 	}
 
+	/*
+	 * Sets Motor Power For Backl motor for Drivetrain
+	 * 
+	 * @param Driver
+	 * 
+	 * @author Nicholas Blackburn
+	 */
 	public void setBackLDrive(final double power) {
 		backLDrive.set(power);
 	}
@@ -264,11 +364,11 @@ public class Drivetrain extends SubsystemBase {
 		zeroEncoders();
 	}
 
-	/*public void holdPosition() {
-		final double left = leftEncoder.getDistance() * 217.3 * Constants.PHold;
-		final double right = rightEncoder.getDistance() * 217.3 * Constants.PHold;
-		setLeftRightPower(left, right);
-	}*/
+	/*
+	 * public void holdPosition() { final double left = leftEncoder.getDistance() *
+	 * 217.3 * Constants.PHold; final double right = rightEncoder.getDistance() *
+	 * 217.3 * Constants.PHold; setLeftRightPower(left, right); }
+	 */
 
 	public void arcadeDrive(final double throttle, final double turn) {
 		drive.arcadeDrive(throttle, turn);
@@ -301,7 +401,7 @@ public class Drivetrain extends SubsystemBase {
 		arcadeDrive(-throttle, turn);
 	}
 
-	public void setHoldPosition() { 
+	public void setHoldPosition() {
 		zeroSensors();
 		drivePID.setDesiredValue(0);
 		turnPID.setDesiredValue(0);
--- 5740-2020-Robot/src/main/java/frc/robot/util/PID.java
+++ 5740-2020-Robot/src/main/java/frc/robot/util/PID.java
@@ -1,231 +1,199 @@
-package frc.robot.util;
-
 /*
  * PID.java
  * A PID feedback controller.
  * @author Julia Cecchetti
  */
+package frc.robot.util;
 
-public class PID{
-	double m_p;   // P coefficient
-	double m_i;   // I coefficient
-	double m_d;   // D coefficient
+public class PID {
+	double m_p; // P coefficient
+	double m_i; // I coefficient
+	double m_d; // D coefficient
 
 	double m_desiredValue; // Desired value
 	double m_previousValue; // Value at last call
 	int m_errorSum; // Sum of previous errors (for I calculation)
 	int m_errorIncrement; // Max increment to error sum each call
 	double m_errorEpsilon; // Allowable error in determining when done
-	
+
 	boolean m_firstCycle; // Flag for first cycle
 	double m_maxOutput; // Ceiling on calculation output
 
 	int m_minCycleCount; // Minimum number of cycles in epsilon range to be done
 	int m_cycleCount; // Current number of cycles in epsilon range
 
-/**
- * Initializes the PID object. All parameters default to 0.
- */
-public PID (double p, double i, double d, double epsilon){
-	m_p = p;
-	m_i = i;
-	m_d = d;
+	/**
+	 * Initializes the PID object. All parameters default to 0.
+	 */
+	public PID(double p, double i, double d, double epsilon) {
+		m_p = p;
+		m_i = i;
+		m_d = d;
 
-	m_errorEpsilon = epsilon;
-	m_desiredValue = 0; // Default to 0, set later by the user
-	m_firstCycle = true;
-	m_maxOutput = 1; // Default to full range
-	m_errorIncrement = 1;
+		m_errorEpsilon = epsilon;
+		m_desiredValue = 0; // Default to 0, set later by the user
+		m_firstCycle = true;
+		m_maxOutput = 1; // Default to full range
+		m_errorIncrement = 1;
 
-	m_cycleCount = 0;
-	m_minCycleCount = 10; // Default
-}
-
-/**
- * Sets the PID constants to new values.
- */
-public void setConstants(double p, double i, double d)
-{
-	m_p = p;
-	m_i = i;
-	m_d = d;
-	
-}
-
-/**
- * Sets the allowable error range away from the desired value.
- */
-public void setErrorEpsilon(int epsilon)
-{
-	m_errorEpsilon = epsilon;
-}
-
-/**
- * Sets the maximum increment to the error sum used in the I component
- * calculation.
- * This defaults to 1 in the constructor, which has worked well for 1114 the
- * past few years.
- */
-public void setErrorIncrement(int inc)
-{
-	m_errorIncrement = inc;
-}
-
-/**
- * Sets the desired value.
- */
-public void setDesiredValue(double val)
-{
-	m_desiredValue = val;
-}
-
-/**
- * Sets the ceiling for the output of the calculation.
- * This defaults to 1.0 (full output). Values should be between 0.0 and 1.0.
- */
-public void setMaxOutput(double max)
-{	if(max >= 0.0 && max <= 1.0)
-	{
-		m_maxOutput = max;
+		m_cycleCount = 0;
+		m_minCycleCount = 10; // Default
 	}
-}
 
-/**
- * Resets the error sum back to zero.
- */
-public void resetErrorSum()
-{
-	m_errorSum = 0;
-}
+	/**
+	 * Sets the PID constants to new values.
+	 */
+	public void setConstants(double p, double i, double d) {
+		m_p = p;
+		m_i = i;
+		m_d = d;
 
-/**
- * Calculates the PID output based on the current value.
- * PID constants and desired value should be set before calling this
- * function.
- */
-public double calcPID(double currentValue)
-{	
-	// Initialize all components to 0.0 to start.
-	double pVal = 0;
-	double iVal = 0;
-	double dVal = 0;
-		
-	// Don't apply D the first time through.
-	if(m_firstCycle)
-	{	
-		m_previousValue = currentValue;  // Effective velocity of 0
-		m_firstCycle = false;
 	}
-	
-	// Calculate P Component.
-	double error = m_desiredValue - currentValue;
-	pVal = m_p * (double)error;
-	
-	// Calculate I Component.
-	// Error is positive and outside the epsilon band.
-	if(error >= m_errorEpsilon)
-	{	
-		// Check if epsilon was pushing in the wrong direction.
-		if(m_errorSum < 0)
-		{
-			// If we are fighting away from the point, reset the error.
-			m_errorSum = 0;
-		}
-		if(error < m_errorIncrement)
-		{
-			// If the error is smaller than the max increment amount, add it.
-			m_errorSum += error;
-		}
-		else 
-		{
-			// Otherwise, add the maximum increment per cycle.
-			m_errorSum += m_errorIncrement;      
+
+	/**
+	 * Sets the allowable error range away from the desired value.
+	 */
+	public void setErrorEpsilon(int epsilon) {
+		m_errorEpsilon = epsilon;
+	}
+
+	/**
+	 * Sets the maximum increment to the error sum used in the I component
+	 * calculation. This defaults to 1 in the constructor, which has worked well for
+	 * 1114 the past few years.
+	 */
+	public void setErrorIncrement(int inc) {
+		m_errorIncrement = inc;
+	}
+
+	/**
+	 * Sets the desired value.
+	 */
+	public void setDesiredValue(double val) {
+		m_desiredValue = val;
+	}
+
+	/**
+	 * Sets the ceiling for the output of the calculation. This defaults to 1.0
+	 * (full output). Values should be between 0.0 and 1.0.
+	 */
+	public void setMaxOutput(double max) {
+		if (max >= 0.0 && max <= 1.0) {
+			m_maxOutput = max;
 		}
 	}
-	// Error is negative and outside the epsilon band.
-	else if(error <= -m_errorEpsilon)
-	{	
-		if(m_errorSum > 0)
-		{
-			// If we are fighting away from the point, reset the error.
-			m_errorSum = 0;
-		}
-		// error is small than max contribution -> just subtract error amount
-		if(error > -m_errorIncrement)
-		{
-			// If the error is smaller than the max increment amount, add it.
-			m_errorSum += error; // Error is negative
-		}
-		else
-		{
-			// Otherwise, subtract the maximum increment per cycle.
-			m_errorSum -= m_errorIncrement;
-		}
-	}
-	// Error is inside the epsilon band. 
-	else
-	{
+
+	/**
+	 * Resets the error sum back to zero.
+	 */
+	public void resetErrorSum() {
 		m_errorSum = 0;
 	}
-	iVal = m_i * (double)m_errorSum;
-	
-	// Calculate D Component.
-	double velocity = currentValue - m_previousValue;
-	dVal = m_d * (double)velocity;
 
-	// Calculate and limit the ouput: Output = P + I - D
-	double output = pVal + iVal - dVal;
-	if(output > m_maxOutput)
-	{
-		output = m_maxOutput;
-	}
-	else if(output < -m_maxOutput)
-	{
-		output = -m_maxOutput;
-	}
-	
-	// Save the current value for next cycle's D calculation.
-	m_previousValue = currentValue;
-	
-	return output;
-}
+	/**
+	 * Calculates the PID output based on the current value. PID constants and
+	 * desired value should be set before calling this function.
+	 */
+	public double calcPID(double currentValue) {
+		// Initialize all components to 0.0 to start.
+		double pVal = 0;
+		double iVal = 0;
+		double dVal = 0;
 
-/**
- * Sets the minimum number of cycles the value must be in the epsilon range
- * before the system is considered stable.
- */
-public void setMinDoneCycles(int n)
-{
-	m_minCycleCount = n;
-}
-
-/**
- * Returns true if the last input was within the epsilon range of the
- * destination value, and the system is stable.
- */
-public boolean isDone()
-{	
-	
-	if (m_previousValue <= m_desiredValue + m_errorEpsilon
-			&& m_previousValue >= m_desiredValue - m_errorEpsilon
-			&& !m_firstCycle)
-	{
-		if(m_cycleCount >= m_minCycleCount)
-		{
-
-			m_cycleCount = 0;
-			
-			return true;
-		
+		// Don't apply D the first time through.
+		if (m_firstCycle) {
+			m_previousValue = currentValue; // Effective velocity of 0
+			m_firstCycle = false;
 		}
-		else 
-		{	
-			m_cycleCount++;
+
+		// Calculate P Component.
+		double error = m_desiredValue - currentValue;
+		pVal = m_p * (double) error;
+
+		// Calculate I Component.
+		// Error is positive and outside the epsilon band.
+		if (error >= m_errorEpsilon) {
+			// Check if epsilon was pushing in the wrong direction.
+			if (m_errorSum < 0) {
+				// If we are fighting away from the point, reset the error.
+				m_errorSum = 0;
+			}
+			if (error < m_errorIncrement) {
+				// If the error is smaller than the max increment amount, add it.
+				m_errorSum += error;
+			} else {
+				// Otherwise, add the maximum increment per cycle.
+				m_errorSum += m_errorIncrement;
+			}
 		}
+		// Error is negative and outside the epsilon band.
+		else if (error <= -m_errorEpsilon) {
+			if (m_errorSum > 0) {
+				// If we are fighting away from the point, reset the error.
+				m_errorSum = 0;
+			}
+			// error is small than max contribution -> just subtract error amount
+			if (error > -m_errorIncrement) {
+				// If the error is smaller than the max increment amount, add it.
+				m_errorSum += error; // Error is negative
+			} else {
+				// Otherwise, subtract the maximum increment per cycle.
+				m_errorSum -= m_errorIncrement;
+			}
+		}
+		// Error is inside the epsilon band.
+		else {
+			m_errorSum = 0;
+		}
+		iVal = m_i * (double) m_errorSum;
+
+		// Calculate D Component.
+		double velocity = currentValue - m_previousValue;
+		dVal = m_d * (double) velocity;
+
+		// Calculate and limit the ouput: Output = P + I - D
+		double output = pVal + iVal - dVal;
+		if (output > m_maxOutput) {
+			output = m_maxOutput;
+		} else if (output < -m_maxOutput) {
+			output = -m_maxOutput;
+		}
+
+		// Save the current value for next cycle's D calculation.
+		m_previousValue = currentValue;
+
+		return output;
 	}
-	
-	//m_cycleCount = 0;		
-	
-	return false;
-}
+
+	/**
+	 * Sets the minimum number of cycles the value must be in the epsilon range
+	 * before the system is considered stable.
+	 */
+	public void setMinDoneCycles(int n) {
+		m_minCycleCount = n;
+	}
+
+	/**
+	 * Returns true if the last input was within the epsilon range of the
+	 * destination value, and the system is stable.
+	 */
+	public boolean isDone() {
+
+		if (m_previousValue <= m_desiredValue + m_errorEpsilon && m_previousValue >= m_desiredValue - m_errorEpsilon
+				&& !m_firstCycle) {
+			if (m_cycleCount >= m_minCycleCount) {
+
+				m_cycleCount = 0;
+
+				return true;
+
+			} else {
+				m_cycleCount++;
+			}
+		}
+
+		// m_cycleCount = 0;
+
+		return false;
+	}
 }
